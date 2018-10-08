360d23ddc55248e80cf454dd8d48ca9153506a8f
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-10-07 17:21:42 -0700

    show counties

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,6 @@
 class MapsController < ApplicationController
   def index
     @states = 'https://www.nohrsc.noaa.gov/data/vector/master/st_us.kmz'
+    @counties = 'https://www.nohrsc.noaa.gov/data/vector/master/cnt_us.kmz'
   end
 end
