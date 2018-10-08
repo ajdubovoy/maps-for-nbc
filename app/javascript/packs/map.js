b6aaf84a859fa95a7f7f181faccd21841abd0ffe
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-10-07 17:15:08 -0700

    display states

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,4 +4,10 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
     const map = new GMaps({ el: '#map', lat: 39.8283, lng: -98.579});
     map.setZoom(4);
+   var statesLayer = new google.maps.KmlLayer({
+     url: mapElement.dataset.states,
+     suppressInfoWindows: true,
+     preserveViewport: false,
+     map: map.map
+   });
 }
